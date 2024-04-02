55b04d08edf81095bb42a66e38b7a29d28a17932
Cicero Alves <cicero.alves.ext@telefonica.com>
2024-04-02 16:07:41 -0300

    ola

--- src/App.tsx
+++ src/App.tsx
@@ -78,7 +78,7 @@ export default function App() {
     try {
       html5QrCode
         .start(
-          videoDevices[2].deviceId,
+          videoDevices[1].deviceId,
           {
             fps: 10,
             
