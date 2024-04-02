ff5a803b4a77dfebd491acc00f9d2369072d32af
Cicero Alves <cicero.alves.ext@telefonica.com>
2024-04-02 16:30:41 -0300

    ola

--- src/App.tsx
+++ src/App.tsx
@@ -56,7 +56,7 @@ export default function App() {
       const stream = await navigator.mediaDevices.getUserMedia({ video: { deviceId: { exact: device.deviceId } } });
       stream.getVideoTracks().forEach(track => {
         const capabilities = track.getCapabilities();
-        devId += JSON.stringify( capabilities);
+        devId += JSON.stringify( capabilities.focusMode);
         console.log(capabilities);
         const settings = track.getSettings();
         // console.log(settings);
