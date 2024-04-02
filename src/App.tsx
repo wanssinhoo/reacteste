3337d8cca38e734fe9a3e124079a346b69015165
Cicero Alves <cicero.alves.ext@telefonica.com>
2024-04-02 13:16:03 -0300

    ola

--- src/App.tsx
+++ src/App.tsx
@@ -1,147 +1,3 @@
-// import React from 'react';
-// import { Html5Qrcode, Html5QrcodeSupportedFormats } from 'html5-qrcode';
-// import { Box, ButtonFixedFooterLayout, ButtonSecondary, Text4, ThemeContextProvider, VIVO_NEW_SKIN, getSkinByName, skinVars } from '@telefonica/mistica';
-// import styles from './styles.module.css';
-
-// export default function App() {
-//   const width = global.window.innerWidth;
-//   const height = global.window.innerHeight;
-
-//   const aspectRatio = width / height;
-
-//   const onNewScanResult = (decodedText: string) => {
-//     window.alert(decodedText);
-//   }
-
-//   const handleStartScanning = async (html5QrCode: Html5Qrcode) => {
-//     const userAgent = navigator.userAgent;
-//     const videoStream = await window.navigator.mediaDevices.getUserMedia({
-//       video: {
-//         facingMode: 'environment',
-//         aspectRatio,
-//       },
-//       audio: false
-//     });
-
-//     try {
-//       html5QrCode
-//         .start(
-//           videoStream.getVideoTracks()[0].id,
-//           {
-//             fps: 10,
-//             videoConstraints: {
-//               facingMode: 'environment',
-//               zoom: userAgent.match(/IPHONE/i) ? 4 : 1.5,
-//               aspectRatio,
-//             },
-//           },
-//           onNewScanResult,
-//           () => {
-//             return;
-//           },
-//         )
-//         .catch(() => {
-//           console.error('VOCÊ NÃO TEM PERMISSÃO');
-//         });
-//     } catch {
-//       if (userAgent.match(/Android/i))
-//         console.log('ACEITE A PERMISSÃO');
-//       else {
-//         console.error('VOCÊ NÃO TEM PERMISSÃO');
-//       }
-//     }
-//   };
-
-//   React.useEffect(() => {
-//     const html5QrCode = new Html5Qrcode('video-area', {
-//       useBarCodeDetectorIfSupported: true,
-//       verbose: false,
-//       formatsToSupport: [Html5QrcodeSupportedFormats.CODE_128],
-//     });
-
-//     handleStartScanning(html5QrCode);
-
-//     return () => {
-//       html5QrCode
-//         .stop()
-//         .then(() => {
-//           return;
-//         })
-//         .catch(() => {
-//           return;
-//         });
-//     };
-//     // eslint-disable-next-line react-hooks/exhaustive-deps
-//   }, []);
-
-//   return (
-//     <ThemeContextProvider theme={{
-//       skin: getSkinByName(VIVO_NEW_SKIN),
-//       colorScheme: 'light',
-//       i18n: {
-//         locale: 'pt-BR',
-//         phoneNumberFormattingRegionCode: 'BR',
-//       },
-
-//     }}>
-//       <ButtonFixedFooterLayout
-//         isFooterVisible
-//         containerBgColor={'#000000'}
-//         footerBgColor={'#000000'}
-//         button={
-//           <ButtonSecondary
-//             onPress={() => {
-//               window.alert(aspectRatio);
-//             }}
-//             style={{
-//               background:
-//                 skinVars.colors.buttonSecondaryBorderInverse,
-//             }}
-//           >
-//             {'Digitar código'}
-//           </ButtonSecondary>
-//         }
-//       >
-//         <Box className={styles.overlay}>
-//           <Box className={styles.header}>
-//             <Box className={styles.info_header}>
-//               <Text4
-//                 textAlign="center"
-//                 medium
-//                 color={skinVars.colors.textPrimaryInverse}
-//               >
-//                 {'Posicione o código de barras na linha'}
-//               </Text4>
-//               <Text4
-//                 textAlign="center"
-//                 medium
-//                 color={skinVars.colors.textPrimaryInverse}
-//               >
-//                 {'e aguarde a leitura automática'}
-//               </Text4>
-//             </Box>
-//           </Box>
-//           <Box className={styles.scanning_area}>
-//             <div
-//               id="video-area"
-//               data-testid="video"
-//               className={styles.videos}
-//             />
-//             <hr
-//               className={styles.divider}
-//               style={{
-//                 backgroundColor: `${skinVars.colors.buttonPrimaryBackground}`,
-//                 borderColor: `${skinVars.colors.buttonPrimaryBackground}`,
-//               }}
-//             />
-//           </Box>
-//         </Box>
-//       </ButtonFixedFooterLayout>
-//     </ThemeContextProvider>
-//   );
-// }
-
-
 import React from 'react';
 import { Html5Qrcode, Html5QrcodeSupportedFormats } from 'html5-qrcode';
 import { Box, ButtonFixedFooterLayout, ButtonSecondary, Text4, ThemeContextProvider, VIVO_NEW_SKIN, getSkinByName, skinVars } from '@telefonica/mistica';
@@ -150,8 +6,9 @@ import styles from './styles.module.css';
 export default function App() {
   const width = global.window.innerWidth;
   const height = global.window.innerHeight;
-  let dev = "";
+  let dev: any;
   let devId: any;
+  let videoStream: MediaStream;
 
   const aspectRatio = width / height;
 
@@ -161,78 +18,59 @@ export default function App() {
 
   const handleStartScanning = async (html5QrCode: Html5Qrcode) => {
     const userAgent = navigator.userAgent;
-    const videoStream = await window.navigator.mediaDevices.getUserMedia({
+    videoStream = await window.navigator.mediaDevices.getUserMedia({
       video: {
-        facingMode: 'environment',
+        facingMode: { ideal: 'environment' },
       },
-      audio: false
-    })
-    // .then((stream) => {
-    //   video.srcObject = stream;
-    //   let track = stream.getVideoTracks()[0];
-    //   track.applyConstraints({
-    //     advanced: [
-    //       { focusMode: 'manual', focusDistance: 0.33 }
-    //     ]
-    //   })
-    // });
+      audio: false,
+    });
+      devId = await window.navigator.mediaDevices.enumerateDevices();
 
-    
 
-    let devi = await window.navigator.mediaDevices.enumerateDevices();
-    // let devis = await window.navigator.mediaDevices;
-    
-    // devis.applyConstraints({});
-    // dev = devi.length + " ";
-    let videoDevices: Array<MediaDeviceInfo> = [];
-    // let videoDevicesOk: any;
+    // if (videoStream.getVideoTracks().length == 1) {
+    //   devId = videoStream.getVideoTracks()[0].getCapabilities().deviceId;
+    // } else {
+    //   videoStream.getVideoTracks().forEach(track => {
+    //     const capabilities = track.getCapabilities();
+    //     console.log(capabilities);
+    //     const settings = track.getSettings();
+    //     // console.log(settings);
+    //     console.log('');
+    //   });
+    // }
+
+    // dev = videoStream.getTracks();
+    // let devi = await window.navigator.mediaDevices.enumerateDevices();
+    // let videoDevices: Array<MediaDeviceInfo> = [];
     // await devi.forEach((device: MediaDeviceInfo) => {
-    //   if (device.kind == 'videoinput' && device.label.match(/back/i)) {
+    //   if (device.kind == 'videoinput') {
     //     videoDevices.push(device);
     //   }
     // });
-    
-    // dev += JSON.stringify(videoStream.getVideoTracks());
-    
+
     // for (let i in videoDevices) {
     //   const device = videoDevices[i];
-    //   // dev += "Opening video device " + device.deviceId + " (" + device.label + ")" ;
-    //   const stream = await window.navigator.mediaDevices.getUserMedia({ video: { deviceId:  device.deviceId} });
-    //   stream.getVideoTracks()[i].getCapabilities();
-    //   if(stream.getVideoTracks()[i].getCapabilities().facingMode?.indexOf("continuous") != -1){
-    //     devId = device.deviceId;
-    //     videoDevicesOk = stream.getVideoTracks()[i].id;
-    //   }
-
-
+    //   // console.log( "Opening video device " + device.deviceId + " (" + device.label + ")" );
+    //   const stream = await navigator.mediaDevices.getUserMedia({ video: { deviceId: { exact: device.deviceId } } });
     //   stream.getVideoTracks().forEach(track => {
-    //     const capabilities = track;
-    //     if(capabilities.facingMode?.indexOf("continuous") != -1){
-    //       devId = capabilities.deviceId;
-    //     }
-    //     // console.log(capabilities);
-    //     // const settings = track.getSettings();
-    //     // // console.log(settings);
-    //     // console.log('');
+    //     const capabilities = track.getCapabilities();
+    //     console.log(capabilities);
+    //     const settings = track.getSettings();
+    //     // console.log(settings);
+    //     console.log('');
     //   }
     //   )
 
-    //   // stream.getTracks().forEach(track => track.stop());
+    //   stream.getTracks().forEach(track => track.stop());
     // }
+
+
 
     try {
       html5QrCode
         .start(
-          devi,
-          {
-            fps: 10,
-            videoConstraints: {
-              focusMode: { exact: "continuous" },
-              facingMode: { exact: "environment" },//'environment',
-              // zoom: userAgent.match(/IPHONE/i) ? 4 : 1.5,
-              aspectRatio,
-            },
-          },
+          devId[3],
+          undefined,
           onNewScanResult,
           () => {
             return;
@@ -248,10 +86,6 @@ export default function App() {
         console.error('VOCÊ NÃO TEM PERMISSÃO');
       }
     }
-
-    
-    
-
   };
 
   React.useEffect(() => {
@@ -292,7 +126,7 @@ export default function App() {
         button={
           <ButtonSecondary
             onPress={() => {
-              window.alert(dev);
+              { console.log(videoStream) }
             }}
             style={{
               background:
@@ -312,15 +146,15 @@ export default function App() {
                 medium
                 color={skinVars.colors.textPrimaryInverse}
               >
-                {`ola`}
+                {`${dev}`}
               </Text4>
-              { <Text4
+              {/* <Text4
                 textAlign="center"
                 medium
                 color={skinVars.colors.textPrimaryInverse}
               >
                 {'e aguarde a leitura automática'}
-              </Text4> }
+              </Text4> */}
             </Box>
           </Box>
           <Box className={styles.scanning_area}>
