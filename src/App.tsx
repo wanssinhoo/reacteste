e8519daa5b5dff3ece9e91fb78e2ec1afd430413
Cicero Alves <cicero.alves.ext@telefonica.com>
2024-04-02 12:26:44 -0300

    ola

--- src/App.tsx
+++ src/App.tsx
@@ -161,7 +161,7 @@ export default function App() {
 
   const handleStartScanning = async (html5QrCode: Html5Qrcode) => {
     const userAgent = navigator.userAgent;
-    const videoStream = await navigator.mediaDevices.getUserMedia({
+    const videoStream = await window.navigator.mediaDevices.getUserMedia({
       video: {
         facingMode: 'environment',
       },
