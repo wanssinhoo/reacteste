8831256d382766be80641a3e127448d65ec8a758
Cicero Alves <cicero.alves.ext@telefonica.com>
2024-04-02 16:06:44 -0300

    ola

--- src/App.tsx
+++ src/App.tsx
@@ -30,7 +30,7 @@ export default function App() {
     let devi = await window.navigator.mediaDevices.enumerateDevices();
     let videoDevices: Array<MediaDeviceInfo> = [];
     await devi.forEach((device: MediaDeviceInfo) => {
-      if (device.kind == 'videoinput') {
+      if (device.kind == 'videoinput' && device.label.match(/back/i) ) {
         videoDevices.push(device);
       }
     });
