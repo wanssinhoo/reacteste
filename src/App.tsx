e8f3d99b77658204c1124b8e01638254df1955b4
Cicero Alves <cicero.alves.ext@telefonica.com>
2024-04-03 17:06:06 -0300

    oi

--- src/App.tsx
+++ src/App.tsx
@@ -13,68 +13,157 @@ export default function App() {
     window.alert(decodedText);
   }
 
-  const handleStartScanning = async (html5QrCode: Html5Qrcode) => {
-    let deviceId: string | undefined;
-    const videoDevices: Array<MediaDeviceInfo> = [];
-    const userAgent = navigator.userAgent;
+  const cameraSelectorHandler = async (videoDevices: any): Promise<string | null> => {
+    let deviceId: any;
+    try {
+        if (videoDevices.length === 0) {
+            const videoStream = await window.navigator.mediaDevices.getUserMedia({
+                video: {
+                    facingMode: 'environment',
+                    aspectRatio,
+                },
+                audio: false,
+            });
 
-    const config: any = {
+            deviceId = videoStream.getVideoTracks()[0].id;
+            videoStream.getTracks().forEach(track => track.stop());
+        } else {
+            for (let i in videoDevices) {
+                const device = videoDevices[i];
+                const stream = await navigator.mediaDevices.getUserMedia({ video: { deviceId: { exact: device.deviceId } } });
+                stream.getVideoTracks().forEach(track => {
+                    const capabilities = track.getCapabilities();
+                    if (capabilities.focusMode.indexOf("continuous") != -1)
+                        deviceId = capabilities.deviceId;
+                });
+                stream.getTracks().forEach(track => track.stop());
+            }
+        }
+    } catch (e) {
+        return null;
+    }
+    return deviceId;
+}
+
+const handleStartScanning = async (html5QrCode: Html5Qrcode) => {
+    let deviceId;
+    let config: any = {
         fps: 10,
-        aspectRatio: userAgent.match(/iphone/gi) ? undefined : aspectRatio,
-        videoConstraints: userAgent.match(/iphone/gi)
-            ? {
-                  facingMode: 'environment',
-                  aspectRatio,
-                  zoom: 4,
-              }
-            : 
-            {
-                  facingMode: 'environment',
-                  autoGainControl: true,
-                  zoom: 2,
-              },
     };
 
-    // window.alert(JSON.stringify( config));
 
-    try {
-      const devices = await window.navigator.mediaDevices.enumerateDevices();
+    const userAgent = navigator.userAgent;
+    const videoDevices: Array<MediaDeviceInfo> = [];
 
-      devices.forEach((device: MediaDeviceInfo) => {
-          if (device.label.match(/back/gi)) videoDevices.push(device);
-      });
+    const devices = await window.navigator.mediaDevices.enumerateDevices();
+    devices.forEach((device: MediaDeviceInfo) => {
+        if (device.label.match(/back/gi)) videoDevices.push(device);
+    });
 
-      if(videoDevices.length === 0){
-        const videoStream =
-        await window.navigator.mediaDevices.getUserMedia({
+    console.error('VIDEO: ', videoDevices);
+
+    if (userAgent.match(/iphone/gi)) {
+        const videoStream = await window.navigator.mediaDevices.getUserMedia({
             video: {
-              facingMode: "environment"
+                facingMode: 'environment',
+                aspectRatio,
             },
             audio: false,
         });
-  
-        // window.alert(JSON.stringify( videoStream.getVideoTracks().length));
-        deviceId = videoStream.getVideoTracks()[0].getCapabilities().deviceId;
-        videoStream.getTracks().forEach((track) => track.stop());
-      } else {
-
-        for (const i in videoDevices) {
-          const device = videoDevices[i];
-          const stream = await navigator.mediaDevices.getUserMedia({
-              video: { deviceId: { exact: device.deviceId } },
-          });
-          stream.getVideoTracks().forEach((track) => {
-              const capabilities = track.getCapabilities();
-              if (capabilities.focusMode.indexOf('continuous') != -1)
-                  deviceId = capabilities.deviceId;
-          });
-          stream.getTracks().forEach((track) => track.stop());
-        }
-      }
-
-    } catch {
-      window.alert("ERRO");
+        deviceId = videoStream.getVideoTracks()[0].id;
     }
+    else
+        deviceId = await cameraSelectorHandler(videoDevices);
+
+
+    if (userAgent.match(/iphone/gi)) {
+        config = {
+            ...config,
+            videoConstraints: {
+                aspectRatio,
+                facingMode: 'environment',
+                zoom: 4
+            }
+        }
+    }
+    else {
+        config = {
+            ...config,
+            aspectRatio,
+            videoConstraints: userAgent.match(/android/gi) ? {
+                focusMode: 'continuous',
+                facingMode: 'environment',
+                zoom: 2,
+            } : {
+                facingMode: 'environment',
+                autoGainControl: true,
+                zoom: 2,
+            }
+        };
+    }
+
+  // const handleStartScanning = async (html5QrCode: Html5Qrcode) => {
+  //   let deviceId: string | undefined;
+  //   const videoDevices: Array<MediaDeviceInfo> = [];
+  //   const userAgent = navigator.userAgent;
+
+  //   const config: any = {
+  //       fps: 10,
+  //       aspectRatio: userAgent.match(/iphone/gi) ? undefined : aspectRatio,
+  //       videoConstraints: userAgent.match(/iphone/gi)
+  //           ? {
+  //                 facingMode: 'environment',
+  //                 aspectRatio,
+  //                 zoom: 4,
+  //             }
+  //           : 
+  //           {
+  //                 facingMode: 'environment',
+  //                 autoGainControl: true,
+  //                 zoom: 2,
+  //             },
+  //   };
+
+  //   // window.alert(JSON.stringify( config));
+
+  //   try {
+  //     const devices = await window.navigator.mediaDevices.enumerateDevices();
+
+  //     devices.forEach((device: MediaDeviceInfo) => {
+  //         if (device.label.match(/back/gi)) videoDevices.push(device);
+  //     });
+
+  //     if(videoDevices.length === 0){
+  //       const videoStream =
+  //       await window.navigator.mediaDevices.getUserMedia({
+  //           video: {
+  //             facingMode: "environment"
+  //           },
+  //           audio: false,
+  //       });
+  
+  //       // window.alert(JSON.stringify( videoStream.getVideoTracks().length));
+  //       deviceId = videoStream.getVideoTracks()[0].getCapabilities().deviceId;
+  //       videoStream.getTracks().forEach((track) => track.stop());
+  //     } else {
+
+  //       for (const i in videoDevices) {
+  //         const device = videoDevices[i];
+  //         const stream = await navigator.mediaDevices.getUserMedia({
+  //             video: { deviceId: { exact: device.deviceId } },
+  //         });
+  //         stream.getVideoTracks().forEach((track) => {
+  //             const capabilities = track.getCapabilities();
+  //             if (capabilities.focusMode.indexOf('continuous') != -1)
+  //                 deviceId = capabilities.deviceId;
+  //         });
+  //         stream.getTracks().forEach((track) => track.stop());
+  //       }
+  //     }
+
+  //   } catch {
+  //     window.alert("ERRO");
+  //   }
 
     try {
       html5QrCode
